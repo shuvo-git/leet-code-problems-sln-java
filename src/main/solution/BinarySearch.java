5773102e234d57dfec3713123f6ec4f568fd0725
shuvo-git <shuvo.pma@gmail.com>
2023-02-23 16:15:05 +0600

    Started Solving 1011. Capacity To Ship Packages Within D Days

--- src/main/solution/BinarySearch.java
+++ src/main/solution/BinarySearch.java
@@ -2,7 +2,43 @@ package main.solution;
 
 public class BinarySearch {
 
+    private int bs(int lo, int hi, int[] weights, int days) {
+        if (hi == lo) {
+            return (int) hi;
+        }
+        int mid = (hi + lo) / 2;
+
+        int dayCount = 0;
+        int sum = 0;
+        for (int i = 0; i < weights.length;++i) {
+            sum += weights[i];
+            if (sum > mid) {
+                sum = weights[i];
+                ++dayCount;
+            }
+        }
+        if (sum != 0 && sum < mid) {
+            ++dayCount;
+        }
+
+//        if(dayCount == days){
+//            return (int) mid;
+//        }
+//        else
+            if (dayCount < days) {
+            return bs(lo, mid, weights, days);
+        } else {
+            return bs(mid+1, hi, weights, days);
+        }
+    }
+
+
+    // 1011. [NOT SOLVED] Capacity To Ship Packages Within D Days
     public int shipWithinDays(int[] weights, int days) {
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < weights.length; ++i) {
+            sum += weights[i];
+        }
+        return bs(1, sum, weights, days);
     }
 }
