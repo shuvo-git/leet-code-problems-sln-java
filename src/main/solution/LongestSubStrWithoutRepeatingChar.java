f036c84ba94cf646d56717b6489360e429a7b25b
shuvo-git <shuvo.pma@gmail.com>
2023-10-17 15:01:57 +0600

    leetcode 3

--- src/main/solution/LongestSubStrWithoutRepeatingChar.java
+++ src/main/solution/LongestSubStrWithoutRepeatingChar.java
@@ -7,5 +7,48 @@ package main.solution;
  * @time: 11:48 AM
  */
 
+// leetcode 3
 public class LongestSubStrWithoutRepeatingChar {
+    public static void main(String[] args) {
+        LongestSubStrWithoutRepeatingChar l = new LongestSubStrWithoutRepeatingChar();
+        System.out.println(l.lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(l.lengthOfLongestSubstring("bbbbb"));
+        System.out.println(l.lengthOfLongestSubstring("pwwkew"));
+    }
+
+    public int maxi(int len, int i, int j) {
+        int tmp = j - i;
+        return tmp > len ? tmp : len;
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        int len = 0, sLen
+                = s.length();
+
+        int[] freq = new int[130];
+        int i = 0, j = 0;
+
+        for (; i <= j && j < sLen; ++j) {
+            int c = s.charAt(j);
+            if (freq[c] == 1) {
+                // System.out.println(len+", i="+i+", j="+j);
+                len = maxi(len, i, j);
+
+                while (s.charAt(i) != c) {
+                    int p = s.charAt(i);
+                    --freq[p];
+                    ++i;
+                }
+                ++i;
+                // System.out.println(len+", i="+i+", j="+j);
+
+            } else ++freq[c];
+        }
+
+        // System.out.println(len+", i="+i+", j="+j);
+
+        len = maxi(len, i, j);
+
+        return len;
+    }
 }
