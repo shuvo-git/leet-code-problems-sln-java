ecc1f82c7115c4a4e9a6462ad13402f3f049298b
shuvo-git <shuvo.pma@gmail.com>
2025-09-20 17:07:02 +0600

    [167. Two Sum II - Input Array Is Sorted, TWO POINTER]

--- src/main/leetcode/TwoSum.java
+++ src/main/leetcode/TwoSum.java
@@ -13,10 +13,26 @@ public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> indexMap = new HashMap<>();
         for (int i = 0; i < nums.length; ++i) {
-            if(indexMap.containsKey(target-nums[i]))
-                return new int[]{i,indexMap.get(target-nums[i])};
+            if (indexMap.containsKey(target - nums[i]))
+                return new int[]{i, indexMap.get(target - nums[i])};
 
             indexMap.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+
+    //167. Two Sum II - Input Array Is Sorted ::: SOLVED
+    public int[] twoSum2(int[] numbers, int target) {
+        int i, j;
+        for (i = 0, j = numbers.length - 1; i < j; ) {
+            int left = numbers[i], right = numbers[j];
+            if (left + right == target) {
+                return new int[]{i + 1, j + 1};
+
+            } else if (target > right + left) {  // target-left<right => target<sum
+                i++;
+            } else j--;
         }
 
         return new int[]{};
